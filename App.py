431150cc9893554cbddc010f4f398029fe2a0479
firyaldaffanisrinna <firyaldaffanisrinna@students.unnes.ac.id>
2025-05-22 09:14:04 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,1 @@
-import streamlit as st
-import pandas as pd
-import pickle
-import joblib
-from seabord as sns
-impoert matplotlib.pyplot as plt
-
-st.set_page_config(page_title="iris Dashboard App",layout="centered")
-
-
 
