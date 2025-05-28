bbf4191f9a42df231df3150535a6b3c37d484e30
firyaldaffanisrinna <firyaldaffanisrinna@students.unnes.ac.id>
2025-05-28 11:39:46 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title="Iris Dasboard App", layout="centered")
-st.sidebar.haeder("Dasboard")
 st.title("Selamat Datang di Aplikasi Proyek Data Mining")
 st.write("Ini adalah aplikasi Streamlit pertamaku")
