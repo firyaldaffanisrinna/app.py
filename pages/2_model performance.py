e8e1f6514f9126a5dbc868e7e8a5b58947ccf4ca
firyaldaffanisrinna <firyaldaffanisrinna@students.unnes.ac.id>
2025-05-28 11:32:35 +0700

    Update 2_model performance.py

--- pages/2_model performance.py
+++ pages/2_model performance.py
@@ -1,15 +1,3 @@
 import streamlit as st
-import pandas as pd
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
-
-st.set_page_config(page_title="Prediction")
-st.hest.set_page_config(page_title=Noneder("Prediction")
-
-st.load Dataset
-df = pd.read_csv("model/iris.csv")
-dataset - pd.read_cvs("https://gist.githubusercontent.com
-
-Tampilkan Dataframe
-st.subheader("Iris Dataset")
+st.set_page_config(page_title="Model performance"
+st.title("data apa gitu")
